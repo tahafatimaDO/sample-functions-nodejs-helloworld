6f64e1c5e47fc307837bb2b263ad05b727ddddc2
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2022-05-25 12:10:18 +0530

    Delete packages/sample/hello directory

--- packages/sample/hello/test/hello.spec.js
+++
@@ -1,8 +0,0 @@
-const { main } = require("../hello");
-
-describe("Test App", () => {
-  test("Hello", () => {
-    let response = main({ name: "test_name" });
-    expect(response.body).toEqual("Hello test_name!");
-  });
-});
